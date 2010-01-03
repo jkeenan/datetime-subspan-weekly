4bc4e384efc2a73b9b288a2786f4d2ce9489001b
jkeenan <jkeenan@cpan.org>
2010-01-03 10:19:26 -0500

    First draft of lib/DateTime/Subspan/Weekly.pm and Makefile.PL.

---
+++ lib/DateTime/Subspan/Weekly.pm
@@ -0,0 +1,80 @@
+package DateTime::Subspan::Weekly;
+use strict;
+
+BEGIN {
+    use Exporter ();
+    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+    $VERSION     = '0.01';
+    @ISA         = qw(Exporter);
+    #Give a hoot don't pollute, do not export more than needed by default
+    @EXPORT      = qw();
+    @EXPORT_OK   = qw();
+    %EXPORT_TAGS = ();
+}
+
+
+#################### main pod documentation begin ###################
+## Below is the stub of documentation for your module. 
+## You better edit it!
+
+
+=head1 NAME
+
+DateTime::Subspan::Weekly - Determine whether event falls within weekly recurring range
+
+=head1 SYNOPSIS
+
+  use DateTime::Subspan::Weekly;
+  blah blah blah
+
+
+=head1 DESCRIPTION
+
+Stub documentation for this module was created by ExtUtils::ModuleMaker.
+It looks like the author of the extension was negligent enough
+to leave the stub unedited.
+
+Blah blah blah.
+
+
+=head1 USAGE
+
+
+
+=head1 BUGS
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+    James E Keenan
+    CPAN ID: jkeenan
+    Perl Seminar NY
+    jkeenan@cpan.org
+    http://thenceforward.net/perl/modules/DateTime-Subspan-Weekly/
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+
+#################### main pod documentation end ###################
+
+
+1;
+# The preceding line will help the module return a true value
+
